9a9f5a1cb7e06a1580eba59c8477d60010ec8754
nuuuwan <nuuuwan@gmail.com>
2024-09-01 10:14:58 +0530

    Add Data of week

--- src/view/molecules/CustomDatePicker.js
+++ src/view/molecules/CustomDatePicker.js
@@ -4,9 +4,14 @@ import React from "react";
 export default function CustomDatePicker({ dateList, currentDate, setDate }) {
   const valueInit = dateList.indexOf(currentDate);
   const [value, setValue] = React.useState(valueInit);
+
+  const date = new Date(dateList[value]);
   return (
     <Box>
-      <Typography variant="h4">{dateList[value]}</Typography>
+      <Typography variant="h4">{date.toLocaleDateString(
+        undefined,
+        { year: "numeric", month: "short", day: "numeric" , weekday: "short", }
+      )}</Typography>
       <Slider
         value={value}
         onChange={(event, value) => setValue(value)}
